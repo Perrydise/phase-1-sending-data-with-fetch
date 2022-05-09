269918cd6eb98a9a00b595466b53ec507a5c91a2
Perry Jorss <pjorss123@gmail.com>
2022-05-09 17:36:40 -0400

    commit tree 6aa9f84da032fbd4f5b55b6d1759fed0003b6064

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(currentName, currentEmail) {
+    const dataBody = {name: currentName, email: currentEmail}
+    const userInfo = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(dataBody),
+    }
+ return fetch("http://localhost:3000/users", userInfo)
+    .then ((response) => response.json()
+    )
+    .then((userInfo) => appendData(userInfo.id)
+    )
+    .catch((errorObject) => appendData(errorObject.message))
+
+}
+
+function appendData(messageString){
+   const header = document.createElement("h1")
+   header.innerHTML = messageString
+   document.body.appendChild(header)
+}
+
+
+
+
+
